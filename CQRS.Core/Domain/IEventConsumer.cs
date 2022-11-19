f932407800a4f4c02a60a397bac1c294a4a89fd5
Phạm Duy Hoàng <57904223+duyhoang5891@users.noreply.github.com>
2022-11-19 08:59:26 +0700

    Implement consumer

---
+++ CQRS.Core/Domain/IEventConsumer.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace CQRS.Core.Domain
+{
+    public interface IEventConsumer
+    {
+        void Consume(string topic);
+    }
+}
+
---
+++ Nemo.Query.Infratructure/Coverters/EventJsonConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using CQRS.Core.Events;
+
+namespace Nemo.Query.Infratructure.Coverters
+{
+    public class EventJsonConverter:JsonConverter<BaseEvent>
+    {
+        public EventJsonConverter()
+        {
+        }
+
+        public override bool CanConvert(Type typeToConvert)
+        {
+            return typeToConvert.IsAssignableFrom(typeof(BaseEvent));
+        }
+
+        public override BaseEvent? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Write(Utf8JsonWriter writer, BaseEvent value, JsonSerializerOptions options)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
